15ec5062dd966a39b612feb7b6a69ac6dc77e5f7
Matteo Vigoni <madcat4623@gmail.com>
2022-07-31 18:16:36 +0200

    Update 3

---
+++ build/precache-manifest.0e11fb8b9d0015583d358b82a1c56eee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2760d08907ae8c461ba1",
+    "url": "/Daishi96/robofriends/static/css/main.02c9e5a4.chunk.css"
+  },
+  {
+    "revision": "2760d08907ae8c461ba1",
+    "url": "/Daishi96/robofriends/static/js/main.2760d089.chunk.js"
+  },
+  {
+    "revision": "0bf54a1201dab9a9b136",
+    "url": "/Daishi96/robofriends/static/js/1.0bf54a12.chunk.js"
+  },
+  {
+    "revision": "be5842a6535c70e032fe",
+    "url": "/Daishi96/robofriends/static/css/2.2d122cd6.chunk.css"
+  },
+  {
+    "revision": "be5842a6535c70e032fe",
+    "url": "/Daishi96/robofriends/static/js/2.be5842a6.chunk.js"
+  },
+  {
+    "revision": "298634686d7588db914e",
+    "url": "/Daishi96/robofriends/static/js/runtime~main.29863468.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Daishi96/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "70f632d7c6829dc2f6bde329be07597b",
+    "url": "/Daishi96/robofriends/index.html"
+  }
+];
