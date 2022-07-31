ca58af784ccdae5ac81a2bf76c41cfbfd6bec674
Matteo Vigoni <madcat4623@gmail.com>
2022-07-31 17:58:37 +0200

    Updates

---
+++ precache-manifest.ad1a7d09787e787a45700cc116fb62e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e5f449d6dd0bf342358",
+    "url": "git@github.com:Daishi96/robofriends.git/static/css/main.1f21d4d4.chunk.css"
+  },
+  {
+    "revision": "4e5f449d6dd0bf342358",
+    "url": "git@github.com:Daishi96/robofriends.git/static/js/main.4e5f449d.chunk.js"
+  },
+  {
+    "revision": "0bf54a1201dab9a9b136",
+    "url": "git@github.com:Daishi96/robofriends.git/static/js/1.0bf54a12.chunk.js"
+  },
+  {
+    "revision": "be5842a6535c70e032fe",
+    "url": "git@github.com:Daishi96/robofriends.git/static/css/2.2d122cd6.chunk.css"
+  },
+  {
+    "revision": "be5842a6535c70e032fe",
+    "url": "git@github.com:Daishi96/robofriends.git/static/js/2.be5842a6.chunk.js"
+  },
+  {
+    "revision": "e33d30d62165fccf34a8",
+    "url": "git@github.com:Daishi96/robofriends.git/static/js/runtime~main.e33d30d6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "git@github.com:Daishi96/robofriends.git/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d3bc25c24d21df2ddfc7ac45c3a7a4e1",
+    "url": "git@github.com:Daishi96/robofriends.git/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Daishi96/robofriends/precache-manifest.0e11fb8b9d0015583d358b82a1c56eee.js"
+  "git@github.com:Daishi96/robofriends.git/precache-manifest.ad1a7d09787e787a45700cc116fb62e8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Daishi96/robofriends/index.html", {
+workbox.routing.registerNavigationRoute("git@github.com:Daishi96/robofriends.git/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
