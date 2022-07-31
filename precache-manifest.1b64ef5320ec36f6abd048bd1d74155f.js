e00e9e9223c9625f31713bb0d1eb50f83138c7db
Matteo Vigoni <madcat4623@gmail.com>
2022-07-31 18:20:56 +0200

    Updates

---
+++ precache-manifest.1b64ef5320ec36f6abd048bd1d74155f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7119f0c6617595a59470",
+    "url": "/robofriends/static/css/main.710f0525.chunk.css"
+  },
+  {
+    "revision": "7119f0c6617595a59470",
+    "url": "/robofriends/static/js/main.7119f0c6.chunk.js"
+  },
+  {
+    "revision": "0bf54a1201dab9a9b136",
+    "url": "/robofriends/static/js/1.0bf54a12.chunk.js"
+  },
+  {
+    "revision": "be5842a6535c70e032fe",
+    "url": "/robofriends/static/css/2.2d122cd6.chunk.css"
+  },
+  {
+    "revision": "be5842a6535c70e032fe",
+    "url": "/robofriends/static/js/2.be5842a6.chunk.js"
+  },
+  {
+    "revision": "d9078623073d367ccac9",
+    "url": "/robofriends/static/js/runtime~main.d9078623.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1dcb662b34b2f4d99a2c98b2c3788eca",
+    "url": "/robofriends/index.html"
+  }
+];
